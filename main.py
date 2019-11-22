6e221fe9c5104185193fdedc43a160ca186f2f12
bode <47369659+booude@users.noreply.github.com>
2019-11-22 19:23:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,57 +2,25 @@
 import os
 import PySimpleGUIWeb as sg
 
-#Declarar variáveis de configuração da câmera
-inverter_v = 1
-inverter_h = 1
-opacidade = '255'
-nitidez = '0'
-contraste = '0'
-brilho = '50'
-saturacao = '0'
-iso = '800'
-exposicao = '0'
-abertura = '8000000'
-
-#Condicionais para ativar e desativar espelhamento
-if inverter_v == 1:
-  inverter_v = '-vf'
-elif inverter_v == 0:
-  inverter_v = ''
-else:
-  inverter_v = ''
-
-if inverter_h == 1:
-  inverter_h = '-hf'
-elif inverter_h == 0:
-  inverter_h = ''
-else:
-  inverter_h = ''
-
-#Declarar variável do script que será montado no código
-pipycamera = f'raspistill {inverter_v} {inverter_h} -op {opacidade} -sh {nitidez} -co {contraste} -br {brilho} -sa {saturacao} -ISO {iso} -ev {exposicao} -ss {abertura} -o /home/pi/camera/$DATE.jpg'
-
-#Inserindo valor de data ao nome da foto
-os.system('DATE=$(date +"%Y-%m-%d_%H%M")')
-
-#Utilizando o método system
-"""os.system(pipycamera)"""
-print(pipycamera)
-
 #Interface em PySimpleGUI para configurar a câmera
 layout = [
-  [sg.Text('Opacidade (255)'),sg.InputText('(0-255)')],  [sg.Text('Nitidez (0)'),sg.InputText('(-100-100)')],
-  [sg.Text('Contraste (0)'),sg.InputText('(-100-100)')],
-  [sg.Text('Brilho (50)'),sg.InputText('(0-100)')],
-  [sg.Text('Saturação (0)'),sg.InputText('(-100-100)')],
-  [sg.Text('ISO (8)'),sg.InputText('(100-800)')],
-  [sg.Text('Exposição (0)'),sg.InputText('(-10-10)')],
-  [sg.Text('Abertura (1/125)'),sg.InputText('(~-6000000)')],
-  [sg.Submit(), sg.Cancel()]
+  [sg.Text('Opacidade(255)'),sg.Slider(range=(0,255), default_value=255, size=(10,10), orientation='horizontal',font=('Helvetica', 12)), sg.Text('Nitidez(0)'),sg.Slider(range=(-100,100), default_value=0, size=(10,10), orientation='horizontal', font=('Helvetica', 12))],
+
+  [sg.Text('Contraste(0)'),sg.Slider(range=(-100,100), default_value=0, size=(10,10), orientation='horizontal',font=('Helvetica', 12)), sg.Text('Brilho(50)'),sg.Slider(range=(0,100), default_value=50, size=(10,10), orientation='horizontal', font=('Helvetica', 12))],
+
+  [sg.Text('Saturação(0)'),sg.Slider(range=(-100,100), default_value=0, size=(10,10), orientation='horizontal',font=('Helvetica', 12)), sg.Text('ISO(800)'),sg.Slider(range=(100,800), default_value=800, size=(10,10), orientation='horizontal', font=('Helvetica', 12))],
+
+  [sg.Text('Exposição(0)'),sg.Slider(range=(-10,10), default_value=0, size=(10,10), orientation='horizontal',font=('Helvetica', 12)), sg.Text('Abertura(1/125)'),sg.Slider(range=(-100,100), default_value=8000000, size=(10,10), orientation='horizontal', font=('Helvetica', 12))],
+
+  [sg.Checkbox('Inverter Vertical', default=True)],[sg.Checkbox('Inverter Horizontal', default=True)],
+  
+  [sg.Submit('Capturar'), sg.Cancel('Cancelar')]
 ]
 
 window = sg.Window('PiPy Camera Manager').Layout(layout)
 button, values = window.Read()
+
+#Declarar variáveis de configuração da câmera
 opacidade = values[0]
 nitidez = values[1]
 contraste = values[2]
@@ -61,4 +29,22 @@ saturacao = values[4]
 iso = values[5]
 exposicao = values[6]
 abertura = values[7]
-print(pipycamera)
+inverter_v = values[8]
+inverter_h = values[9]
+
+#Condicionais para ativar e desativar espelhamento
+if inverter_v == True:
+  inverter_v = '-vf'
+else:
+  inverter_v = ''
+
+if inverter_h == True:
+  inverter_h = '-hf'
+else:
+  inverter_h = ''
+
+#Inserindo valor de data ao nome da foto
+os.system('DATE=$(date +"%Y-%m-%d_%H%M")')
+
+#Script que será montado com as variáveis inseridas
+print(f'raspistill {inverter_v} {inverter_h} -op {opacidade} -sh {nitidez} -co {contraste} -br {brilho} -sa {saturacao} -ISO {iso} -ev {exposicao} -ss {abertura} -o /home/pi/camera/$DATE.jpg')
