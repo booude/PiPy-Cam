6b785eaaa257b18ed22a1e209cbd2260fe0777ad
bode <47369659+booude@users.noreply.github.com>
2019-11-23 15:29:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ while True:
     os.system('DATE=$(date +"%Y-%m-%d_%H%M")')
 
 #Montando o script para tirar foto de acordo com os parâmetros informados na interface
-    print("raspistill {} {} -op {} -sh {} -co {} -br {} -sa {} -ISO {} -ev {} -ss {} -o /home/pi/camera/$DATE.jpg" .format(inverter_v,inverter_h,opacidade,nitidez,contraste,brilho,saturacao,iso,exposicao,abertura))
+    os.system("raspistill {} {} -op {} -sh {} -co {} -br {} -sa {} -ISO {} -ev {} -ss {} -o /home/pi/camera/$DATE.jpg" .format(inverter_v,inverter_h,opacidade,nitidez,contraste,brilho,saturacao,iso,exposicao,abertura))
+#     print("raspistill {} {} -op {} -sh {} -co {} -br {} -sa {} -ISO {} -ev {} -ss {} -o /home/pi/camera/$DATE.jpg" .format(inverter_v,inverter_h,opacidade,nitidez,contraste,brilho,saturacao,iso,exposicao,abertura))
 
 window.close()
